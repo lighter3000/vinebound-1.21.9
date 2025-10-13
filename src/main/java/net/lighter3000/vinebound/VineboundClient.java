bd20664e479d6d7da8ab8561782e7b56478c9998
lighter3000 <tom.bohne1@gmail.com>
2025-10-13 14:19:48 +0200

    Initial commit

---
+++ src/main/java/net/lighter3000/vinebound/VineboundClient.java
@@ -0,0 +1,11 @@
+package net.lighter3000.vinebound;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VineboundClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/lighter3000/vinebound/VineboundDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lighter3000.vinebound;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VineboundDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
