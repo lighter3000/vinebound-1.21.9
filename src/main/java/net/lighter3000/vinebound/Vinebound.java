34241a3d38dce3401aab74d8b74fee76e1400eb0
lighter3000 <tom.bohne1@gmail.com>
2025-10-17 00:04:35 +0200

    # Added to easily add new items
    # Added first Item: ancient_chisel

--- src/main/java/net/lighter3000/vinebound/Vinebound.java
+++ src/main/java/net/lighter3000/vinebound/Vinebound.java
@@ -2,6 +2,7 @@ package net.lighter3000.vinebound;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.lighter3000.vinebound.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,7 @@ public class Vinebound implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItems.registerModItems();
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/lighter3000/vinebound/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.lighter3000.vinebound.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.lighter3000.vinebound.Vinebound;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item ANCIENT_CHISEL = registerItem("ancient_chisel", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Vinebound.MOD_ID,"ancient_chisel")))));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Vinebound.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Vinebound.LOGGER.info("Registering Mod Items for " + Vinebound.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ANCIENT_CHISEL);
+        });
+    }
+}
